81e6d3ddb32da31fb494838397fc6220da9ab0ee
Cheng Vue <csalvue@gmail.com>
2019-01-29 08:49:47 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,6 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    @song.genre = Genre.find(params[:id])
 
     if @song.save
       redirect_to @song
