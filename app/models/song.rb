2deb679c0f7c04743d6ee3cc471fb58201662ccd
Cheng Vue <csalvue@gmail.com>
2019-01-29 07:29:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,8 +3,12 @@ class Song < ActiveRecord::Base
   belongs_to :genre
   has_many :notes
 
-  def artist_name
-    @artist = Artist.find_or_create_by(name: params[:artist_name])
+  def artist_name=(name)
+    @artist = Artist.find_or_create_by(name: name)
     self.artist = @artist
+  end
+
+  def artist_name
+    self.artist.name
   end
 end
